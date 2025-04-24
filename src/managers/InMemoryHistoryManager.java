a23315a441c54e54c3ba0b9199bdc70f7bbc0d17
Nikita Semenov <nik.269@yandex.ru>
2025-04-24 15:46:46 +0300

    Исправление ошибок CheckStyle в Task.java

--- src/managers/InMemoryHistoryManager.java
+++ src/managers/InMemoryHistoryManager.java
@@ -5,7 +5,7 @@ import tasks.Task;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private Map<Integer, Node<Task>> listOfIDTasksInHistory = new HashMap<>();
